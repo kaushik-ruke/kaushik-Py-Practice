2d971adbae2512c4211c5201197ef97cf057dbf0
KAUSHIK <kaushikruke@gmail.com>
2025-10-16 10:10:21 +0530

    DataTypes1

---
+++ DataTypes1.py
@@ -0,0 +1,31 @@
+a = 5
+b = "Sky"
+c = 20.6
+d = 1j  # complex number
+e = ["BMW", "AUDI", "BENZ"]  # list
+f = ("Rohit", "Shreyas", "Sachin")  # tuple
+g = range(4)
+h = {"name": "Kaushik", "Age": 21}  # dictionary
+j = {"apple", "banana", "cherry"}  # set
+k = frozenset({"mango", "grapes", "watermelon"})  # frozen set
+l = True
+m = b"hello"  # bytes
+n = bytearray(5)
+o = memoryview(bytes(5))
+p = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(o))
+print(type(p))
