d8124e552e5378cca5fe58952166a95470fc29e1
KAUSHIK <kaushikruke@gmail.com>
2025-10-16 11:10:09 +0530

    Casting Numbers

---
+++ casting_numbers.py
@@ -0,0 +1,27 @@
+x = int(10)
+y = int(2.8) 
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+#-----------------------------#
+
+a = float(10)
+b = float(2.8) 
+c = float("3")
+
+print(a)
+print(b)
+print(c)
+
+#---------------------------------#
+
+p = str("s1")
+q = str(2)
+r = str(3.0)
+
+print(q)
+print(r)
+print(p)
