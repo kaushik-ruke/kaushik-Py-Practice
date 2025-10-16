8f353de447492ceb86c25c4d872dd212713109b3
KAUSHIK <kaushikruke@gmail.com>
2025-10-16 10:53:27 +0530

    PY_Number

---
+++ py_number.py
@@ -0,0 +1,26 @@
+x = 16  
+y = 28.8 
+z = 1+2j   
+
+a = float(x)
+
+b = int(y)
+
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#-----------------------------------------------------#
+
+import random #this function print Number randomly
+
+print(random.randrange(1, 10))
+print(random.randrange(1, 5))
+print(random.randrange(1, 20))
+
