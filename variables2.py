65b65049ee8fbd7e8ced8198f0b0b22d22e25de1
KAUSHIK <kaushikruke@gmail.com>
2025-10-16 09:32:33 +0530

    Create variables2.py

---
+++ variables2.py
@@ -0,0 +1,27 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#====================================================================#
+
+x = y = z = " Mango "
+print(x)
+print(y)
+print(z)
+
+#=====================================================================#
+
+fruits = ["kiwi", "watermelon", "jack fruit"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#======================================================================================#
+
+x = "India won "
+y = "T20 World Cup "
+z = "And Champions Trophy"
+
+print(x+y+z)
