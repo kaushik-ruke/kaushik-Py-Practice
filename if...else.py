884066d3109d35c973ad84912ed5b29d28c10dbf
KAUSHIK <kaushikruke@gmail.com>
2025-10-17 11:04:14 +0530

    if....else2

--- if...else.py
+++ if...else.py
@@ -7,9 +7,42 @@ if b > a:
 
 #----------------------------------#
 
+# ELIF
+
 a = 33
 b = 33
 if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+
+#-------------------------------------#
+
+#ELSE 
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+#---------------------------------#
+
+if a > b: print("a is greater than b") #short hand if
+print("A") if a > b else print("B") #short hand if....else
+print("A") if a > b else print("=") if a == b else print("B") 
+
+#----------------------------------#
+
+#AND
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")    # in AND condition both value should be correct to get OP true
+elif a > b or a > c:
+  print("At least one of the conditions is True") # in OR condition if the value is one correct then the OP is true
---
+++ if...else2.py
@@ -0,0 +1,31 @@
+# NOT
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+else :
+    print("a is greatest")
+
+#---------------------------------------------#
+
+#NESTED IT
+    
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+    
+#------------------------------------------#
+    
+#PASS STATEMENT
+
+a = 33
+b = 200
+
+if b > a:
+  pass
