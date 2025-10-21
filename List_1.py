16d0b50a33b5cc2ef7067af5202abe133afc0891
KAUSHIK <kaushikruke@gmail.com>
2025-10-21 10:53:08 +0530

    Create List_1.py

---
+++ List_1.py
@@ -0,0 +1,38 @@
+# normal Lists
+
+mylist = ["Max", "Charles", "Lewis"]
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+
+#Allowing Duplicates
+
+thislist1 = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist1)
+
+newlist = ["sameer","kaushik","sachin","kaushik"]
+print(newlist)
+
+
+#Access List
+
+thislist = ["apple", "banana", "cherry","grapes"]
+print(thislist[1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[4:])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:2])
+
+
