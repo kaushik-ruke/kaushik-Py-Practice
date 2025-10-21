c64bb5049471b7d0775296f14ca8888513f27386
KAUSHIK <kaushikruke@gmail.com>
2025-10-21 12:50:08 +0530

    TUPLES 1

--- List_2.py
+++ List_2.py
@@ -126,4 +126,3 @@ list2 = [1, 2, 3]
 
 list1.extend(list2)
 print(list1)
-
---
+++ Tuples1.py
@@ -0,0 +1,20 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple1 = ("apple", "banana", "cherry")
+print(thistuple1[-1])
+
+thistuple2 = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple2[2:5])
+
+thistuple3 = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple3[:4])
+
+thistuple4 = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple4[2:])
+
+thistuple = ("apple", "banana", "cherry")
+if "tomato" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+else:
+    print("not is fruits")
