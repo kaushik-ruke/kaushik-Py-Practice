6c1554bed5fcf9ba3b6db7b97ba8d6480f636b6d
KAUSHIK <kaushikruke@gmail.com>
2025-11-29 09:06:32 +0530

    Update numpy1.py

--- numpy1.py
+++ numpy1.py
@@ -259,5 +259,206 @@ import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
-newarr = arr.reshape(2, 3, 2)
+newarr = arr.reshape(2, 3, 2)  # 2 Columns, 3 Rows and 2 Arrays
 print(newarr)
+
+#------------------------------------#
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+print(arr.reshape(2, 4).base) # will Return as it is for OUTPUT
+
+#----------------------------------------------------#
+
+# Convert into 1d Array
+
+import numpy as np
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+newarr = arr.reshape(-1)
+print(newarr)
+
+
+# Iterating 1d Array
+
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for x in arr:
+  print(x)
+  
+  
+# Iterating as 2d Array
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+  print(x)
+
+
+# iterating 2d arrays on different scalar element.
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+  for y in x:
+    print(y)
+
+
+# Iterating 3d arrays 
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+  print(x)
+
+
+# iterating 3d arrays on different scalar element, Iterate Each Dimension 
+
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+  for y in x:
+    for z in y:
+      print(z)
+
+
+# Iterating Array using nditer()
+
+import numpy as np 
+
+arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+for x in np.nditer(arr):
+  print(x)
+
+
+# Iterate Array using Different Data Types :
+
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+  print(x)
+  
+  
+  
+# Iterating with Different Step Size 
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+for x in np.nditer(arr[:, ::2]):
+  print(x)
+
+#Enumerated Iteration Using ndenumerate()
+
+import numpy as np
+
+arr = np.array([1, 2, 3])
+
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+  
+# Enumerate on following 2D array's elements:
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+
+
+# Joining Numpy Arrays :
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))
+
+print(arr)
+
+
+# Joining 2d arrays as along Axis :
+
+import numpy as np
+
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+arr = np.concatenate((arr1, arr2), axis=1)
+
+print(arr)
+
+
+# Joining Arrays Using Full stack Function
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+
+print(arr)
+
+
+# Array Splitting
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+newarr = np.array_split(arr, 3)
+
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+#----------------------------------------------------------#
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+
+# arrays Search 
+
+import numpy as np
+
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7)
+print(x)
+
+#------------------# 
+
+import numpy as np
+
+arr = np.array([6, 7, 8, 9])
+
+x = np.searchsorted(arr, 7, side='right')
+print(x)
+
+#-----------------------------------------------#
+
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
